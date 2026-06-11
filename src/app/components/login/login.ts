3aa5486acff9bd416f18bb4595bab829507ac4bf
BallaPatrik <99919542+BallaPatrik@users.noreply.github.com>
2026-06-11 16:07:30 +0200

    Added error message display for Login form fields

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -3,7 +3,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '../../services/auth.service';
 import { LocalStorageKeys } from '../../constants/local-storage-keys';
 import { Router } from '@angular/router';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
+import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { FormField, form, minLength, required } from '@angular/forms/signals';
 import { User } from '../../model/user.model';
@@ -12,7 +12,7 @@ import {MessageService} from '../../services/message.service';
 
 @Component({
   selector: 'app-login',
-  imports: [MatButtonModule, MatFormField, MatLabel, MatInput, FormsModule, FormField ],
+  imports: [MatButtonModule, MatFormField, MatLabel, MatInput, FormsModule, FormField, MatError],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
@@ -59,4 +59,38 @@ export class Login implements OnInit {
     minLength(schemaPath.password, 4);
     //differentValidator(schemaPath.name, [schemaPath.description]);
   });
+
+  getNameErrorMessage() {
+    const name = this.loginForm.name();
+
+    if (name.dirty() || name.touched()) {
+      const errors=name.errors();
+
+      if (errors.some(error => error.kind === 'required')) {
+        return 'You must enter a value for name!';
+      }
+
+      if (errors.some(error => error.kind === 'minLength')) {
+        return 'You must enter at least 4 characters for name!';
+      }
+    }
+    return '';
+  }
+
+  getPasswordErrorMessage(){
+    const password = this.loginForm.password();
+
+    if (password.dirty() || password.touched()) {
+      const errors=password.errors();
+
+      if (errors.some(error => error.kind === 'required')) {
+        return 'You must enter a value for password!';
+      }
+
+      if (errors.some(error => error.kind === 'minLength')) {
+        return 'You must enter at least 4 characters for password!';
+      }
+    }
+    return '';
+  }
 }
